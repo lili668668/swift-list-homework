85d5d036d9df4d5bb9da02c9875201811b67dbaa
Tzu-Yin Hong <lili668668@gmail.com>
2019-07-29 19:13:34 +0800

    Initial Commit

---
+++ HomeWork1Tests/HomeWork1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HomeWork1Tests.swift
+//  HomeWork1Tests
+//
+//  Created by Tzu-Yin Hong on 2019/7/29.
+//  Copyright © 2019年 Tzu-Yin Hong. All rights reserved.
+//
+
+import XCTest
+@testable import HomeWork1
+
+class HomeWork1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
